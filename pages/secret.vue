48d8aae7b270528dc5e10ece0f6e052092094e62
jean Tostivint <jean.tostivint@gmail.com>
2019-06-19 17:36:12 +0200

    Interface || v_0_1__1

--- pages/secret.vue
+++ pages/secret.vue
@@ -1,15 +1,43 @@
 <template>
   <div>
-    <h1>Super secret page</h1>
-    <p>If you try to access this URL not connected, you will see the error page telling your that you are not connected.</p>
-    <NuxtLink to="/">
-      Back to the home page
-    </NuxtLink>
+    <h1>Editeur d'article</h1>
+
+    <select v-model="selected">
+      <option value>Choississez un article</option>
+      <option v-for="select in articles" v-bind:key="select._id">{{ select.title }}</option>
+    </select>
+
+    {{ selected }}
+    
+    <NuxtLink to="/">Back to the home page</NuxtLink>
   </div>
 </template>
 
 <script>
 export default {
-  middleware: 'auth'
-}
+  data() {
+    return {
+      articles: null,
+      selected: null,
+    }
+  },
+
+  /* middleware: "auth", */
+
+  methods: {
+    async test() {
+      try {
+        let articles = await this.$store.dispatch("article");
+        this.articles = articles.data.data;
+        console.log(this.articles);
+      } catch (e) {
+        this.formError = e.message;
+      }
+    }
+  },
+
+  mounted: function mounted() {
+    this.test();
+  }
+};
 </script>
