c2ee8a3d376f2691183df132e27b193c380cca14
jean Tostivint <jean.tostivint@gmail.com>
2019-06-21 15:11:39 +0200

    Create article || v_0_5__6

--- pages/administrator/gallery.vue
+++ pages/administrator/gallery.vue
@@ -2,6 +2,36 @@
   <section>
     <h1>Editeur de la galerie</h1>
     <NuxtLink to="/admin">Back to the admin page</NuxtLink>
+
+    <section class="gallery">
+      <br>
+      <form>
+        <input name="filter" placeholder="Filtrer" type="text" autocomplete="off">
+      </form>
+
+      <ul>
+        <li>Agathe</li>
+        <li>Alice</li>
+        <li>Ambre</li>
+        <li>Arthur</li>
+        <li>Camille</li>
+        <li>Chloé</li>
+        <li>Gabin</li>
+        <li>Gabriel</li>
+        <li>Hugo</li>
+        <li>Jade</li>
+        <li>Julia</li>
+        <li>Léa</li>
+        <li>Léo</li>
+        <li>Louis</li>
+        <li>Louise</li>
+        <li>Maël</li>
+        <li>Mila</li>
+        <li>Paul</li>
+        <li>Théo</li>
+        <li>Valentin</li>
+      </ul>
+    </section>
   </section>
 </template>
 
@@ -9,7 +39,31 @@
 export default {
   data() {
     return {
-      gallery: null
+      gallery: null,
+      article: [
+        "Agathe",
+        "Alice",
+        "Ambre",
+        "Arthur",
+        "Camille",
+        "Chloé",
+        "Gabin",
+        "Gabriel",
+        "Hugo",
+        "Jade",
+        "Julia",
+        "Léa",
+        "Léo",
+        "Louis",
+        "Louise",
+        "Maël",
+        "Mila",
+        "Paul",
+        "Théo",
+        "Valentin"
+      ],
+      ul: null,
+      search: null
     };
   },
 
@@ -26,10 +80,8 @@ export default {
 
         console.log(this.gallery);
 
-
         /* Render */
         /* this.componentKey += 1; */
-
       } catch (e) {
         this.formError = e.message;
       }
@@ -37,12 +89,19 @@ export default {
     //
   },
   //
+  created() {},
 
   mounted: function mounted() {
     /*
      * Load articles
      */
     this.loadGallery();
+    this.search = document.querySelector("input");
+    this.ul = document.querySelector("ul");
+
+    this.search.addEventListener("keyup", event => {
+      console.log(event);
+    });
     //
   }
 };
