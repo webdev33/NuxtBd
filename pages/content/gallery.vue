0df1af3753c19106f563866a5bfd7f394e6c25a8
jean Tostivint <jean.tostivint@gmail.com>
2019-06-22 15:38:53 +0200

    Gallery user || v_0_7__3

--- pages/content/gallery.vue
+++ pages/content/gallery.vue
@@ -16,8 +16,33 @@
           type="text"
           autocomplete="off"
         >
+        <input type="checkbox" id="Entrance" value="Entrance" v-model="checkedNames" @change="check">
+        <label for="Entrance">Entrée de station</label>
+
+        <input type="checkbox" id="Station" value="Station" v-model="checkedNames" @change="check">
+        <label for="Station">Station</label>
+
+        <input type="checkbox" id="Train" value="Train" v-model="checkedNames" @change="check">
+        <label for="Train">Train</label>
+
+        <input type="checkbox" id="PostCard" value="PostCard" v-model="checkedNames" @change="check">
+        <label for="PostCard">Cartes postales</label>
+
+        <input type="checkbox" id="Event" value="Event" v-model="checkedNames" @change="check">
+        <label for="Event">Evenement</label>
+
+        <input type="checkbox" id="Document" value="Document" v-model="checkedNames" @change="check">
+        <label for="Train">Train</label>
+
+        <input type="checkbox" id="Building" value="Building" v-model="checkedNames" @change="check">
+        <label for="Building">En construction</label>
       </form>
     </section>
+
+    <div id="example-3">
+      <br>
+      <span>Noms cochés : {{ checkedNames }}</span>
+    </div>
 
     <section class="gallery__form">
       <ul class="gallery__input">
@@ -34,6 +59,8 @@
 export default {
   data() {
     return {
+      checkedNames: [],
+
       /* Search bar */
       gallery: null,
       article: [],
@@ -57,22 +84,32 @@ export default {
         this.gallery = gallery.data.data;
 
         this.gallery.forEach(select => {
-          this.article.push({ legend: select.legend, link: select.link });
+          this.article.push({ legend: select.legend, link: select.link, categorie: JSON.parse(select.categorie) });
         });
 
         /* console.log(JSON.parse(this.gallery[0].categorie)) */
         /* this.gallery.forEach(select => { */
-          /* console.log(JSON.parse(select.categorie)) */
-          /* JSON.parse(select.categorie).forEach(select => {
+        /* console.log(JSON.parse(select.categorie)) */
+        /* JSON.parse(select.categorie).forEach(select => {
             console.log(select);
           }); */
-          /* this.article.push({ legend: select.legend, link: select.link }); */
+        /* this.article.push({ legend: select.legend, link: select.link }); */
         /* }); */
       } catch (e) {
         this.formError = e.message;
       }
-    }
+    },
     //
+
+
+    check() {
+      console.log(this.article)
+
+      this.article.forEach((select) => {
+        console.log(select)
+      });
+      
+    }
   },
 
   mounted: function mounted() {
