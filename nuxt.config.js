502dca1b4402876ece46b25b1cb47bbcbb4034a3
jean Tostivint <jean.tostivint@gmail.com>
2019-06-27 10:33:01 +0200

    Correction (test) || v_0_10__12

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,61 +1,68 @@
-import bodyParser from 'body-parser'
-import session from 'express-session'
+import bodyParser from "body-parser";
+import session from "express-session";
 
 export default {
   head: {
-    title: 'Le père du métro',
+    title: "Le père du métro",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', content: 'Auth Routes example' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", content: "Auth Routes example" }
     ]
   },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: 'favicon.ico',
+    },
+  ],
   /*
-  ** Add server middleware
-  ** Nuxt.js uses `connect` module as server
-  ** So most of express middleware works with nuxt.js server middleware
-  */
+   ** Add server middleware
+   ** Nuxt.js uses `connect` module as server
+   ** So most of express middleware works with nuxt.js server middleware
+   */
   serverMiddleware: [
     // body-parser middleware
     bodyParser.json(),
     // session middleware
     session({
-      secret: 'super-secret-key',
+      secret: "super-secret-key",
       resave: false,
       saveUninitialized: false,
       cookie: { maxAge: 600000 }
     }),
     // Api middleware
     // We add /api/login & /api/logout routes
-    '~/api'
+    "~/api"
   ],
 
-  /* 
-  * Css compilator
-  */
-  css: [
-    
-    '@/assets/scss/style.scss'
-  ],
+  /*
+   * Css compilator
+   */
+  css: ["@/assets/scss/style.scss"],
   //
 
+  /*
+   * Sounds
+   */
   build: {
     loaders: {
       vue: {
         transformAssetUrls: {
-          audio: 'src',
-        },
-      },
+          audio: "src"
+        }
+      }
     },
-
     extend(config, ctx) {
       config.module.rules.push({
         test: /\.(ogg|mp3|wav|mpe?g)$/i,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[path][name].[ext]',
-        },
-      })
-    },
-  },
-}
+          name: "[path][name].[ext]"
+        }
+      });
+    }
+  }
+  //
+};
